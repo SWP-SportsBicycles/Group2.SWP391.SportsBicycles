c3509340ea28d7af53d094b11e49c9dd4598d1ed
TuanETV123 <tuanetv123@gmail.com>
2026-04-15 13:51:30 +0700

    test deploy

--- Group2.SWP391.SportsBicycles.API/Program.cs
+++ Group2.SWP391.SportsBicycles.API/Program.cs
@@ -158,12 +158,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://*:{port}");
+
 
 app.Run();
