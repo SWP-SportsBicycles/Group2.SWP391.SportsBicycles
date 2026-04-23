70b7ce4d6bb15ddcc57c72a26348181bf5b0726a
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2026-04-23 14:38:37 +0700

    FixListingDetail

--- Group2.SWP391.SportsBicycles.Common/DTOs/BuyerBikeListingDTO.cs
+++ Group2.SWP391.SportsBicycles.Common/DTOs/BuyerBikeListingDTO.cs
@@ -14,7 +14,7 @@ namespace Group2.SWP391.SportsBicycles.Common.DTOs
         public decimal Price { get; set; }
         public string Brand { get; set; } = default!;
         public string Category { get; set; } = default!;
-        public string Thumbnail { get; set; } = default!;
+        public string Thumbnail { get; set; } = string.Empty;
         public string Overall { get; set; } = default!;
         public bool IsWishlisted { get; set; }
         public bool IsInspected { get; set; }
