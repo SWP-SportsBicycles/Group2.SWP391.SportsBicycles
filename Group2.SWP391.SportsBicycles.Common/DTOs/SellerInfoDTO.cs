4b9f3ca1e5f0f0aab28702bcd72978d8948ff3b1
nguyenvantoanpr359 <nguyenvantoanpr359@gmail.com>
2026-04-23 15:03:16 +0700

    AddSellerNameInListingDetail

---
+++ Group2.SWP391.SportsBicycles.Common/DTOs/SellerInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2.SWP391.SportsBicycles.Common.DTOs
+{
+    public class SellerInfoDTO
+    {
+        public string SellerName { get; set; } = string.Empty;
+     
+    }
+}
